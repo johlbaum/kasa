3d073c31daefb91270044ab38795f0d162bef5de
johlbaum <julien.ohlbaum@gmail.com>
2023-09-25 23:53:44 +0200

    Error component text content

--- src/Components/Error/index.jsx
+++ src/Components/Error/index.jsx
@@ -1,7 +1,22 @@
+import { Link } from "react-router-dom";
+
+import styles from "./index.module.scss";
+
 function Error() {
   return (
     <div>
-      <h1>Cette page n'existe pas</h1>
+      <p className={styles.codeError}>404</p>
+      <p className={styles.errorTextContent}>
+        Oups! La page que vous demandez n'existe pas.
+      </p>
+
+      <div className={styles.errorLinkContainer}>
+        <Link to="/">
+          <p className={styles.errorHomeLink}>
+            Retourner sur la page d’accueil
+          </p>
+        </Link>
+      </div>
     </div>
   );
 }
